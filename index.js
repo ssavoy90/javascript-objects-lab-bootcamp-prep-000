087a8461db9659543cb900e92b07931dedca274e
Shanelle Savoy <mznellienellz@gmail.com>
2018-05-31 18:41:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function deleteFromObjectByKey(object, key) {
   var newObject = Object.assign({}, object);
   delete newObject.key;
   return newObject
+}
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key;
